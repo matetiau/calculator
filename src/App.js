74eba44768f336739607db4b8686b7d78b0cecf1
matetiau <matekamer@gmail.com>
2018-08-10 16:58:21 +0300

    added pattern to input

--- src/App.js
+++ src/App.js
@@ -17,12 +17,12 @@ class App extends Component {
     super(props);
     
     this.state = {
-     kapko: 0,sum:0,saint:""
+     sum:'',saint:''
     };
    this.addInput = this.addInput.bind(this);
    this.sumApp = this.sumApp.bind(this);
    this.clear = this.clear.bind(this);
-   this.addAns = this.addAns.bind(this);
+
   }
   addInput(){
       document.getElementById('app').onclick = buttonOn => {
@@ -32,45 +32,22 @@ class App extends Component {
 
         if (buttonOn.target.type === "submit" && buttonOn.target.innerText!== "=" ){
           document.getElementById('display').value = document.getElementById('display').value +buttonOn.target.innerText;
-          }
-
-
-
-        /* for operators */
-
-        if (isNaN(buttonOn.target.innerText) && buttonOn.target.innerText !== "=" ){
-          this.setState({saint:buttonOn.target.innerText});
         }
-
-        /* for spec numbers */
-
-        if (!isNaN(buttonOn.target.innerText) && this.state.saint === "+" && this.state.saint !== "=" ){
-          this.setState({kapko:parseFloat(buttonOn.target.innerText)});
-          this.setState({sum: this.state.sum  + this.state.kapko});
-          console.log(this.state.sum);
+        if (buttonOn.target.type === "=" ){
+          this.setState({saint:buttonOn.target.type});
         }
-
-        /* for + */
-
-    
-
-
-
-       
-
-       
-        
-        
-          
-        
           
         
   }
  }
  sumApp(){
+  if (document.getElementById('display').value.split("")[1] === "0"){
+    document.getElementById('display').setAttribute( 'pattern',"^[01-9]" );
+    console.log('error');
+  } else {
   document.getElementById('display').value = eval(document.getElementById('display').value);
+  this.setState({sum:eval(document.getElementById('display').value)});}
   
- 
 
  
  }
@@ -78,23 +55,20 @@ class App extends Component {
 
 
 clear() {
-  document.getElementById('display').setAttribute('placeholder', 0);
-  document.getElementById('display').value = "";
-  this.setState({kapko:""});
+  
+  
   this.setState({sum:0});
+  document.getElementById('display').value = 0;
   
 }
-addAns() {
-  
-  
-  document.getElementById('display').value = this.state.sum;
-}
+
 
 
   render() {
     return (
       <div className="App" style={{fontSize: '50px'}} id="app" onClick={this.addInput}>
-        <input id="display" input="number" step="0.0001"  ></input>
+      <form>
+        <input id="display" step="0.0001" pattern="^[01-9]"  ></input><button id="equals" type="submit" onClick={this.sumApp} >=</button></form>
         
         <div className="numbers" >
         <button id="seven"  >7</button>
@@ -116,10 +90,10 @@ addAns() {
         <button id="subtract">-</button>
         <button id="multiply" >*</button>
         <button id="divide">/</button>
-        <button id="equals" onClick={this.sumApp} >=</button>
-        <button id="ans" style={{fontSize: '30px'}} onClick={this.addAns}>ANS</button>
+        <button id="equals" type="submit" onClick={this.sumApp} >=</button>
+     
         </div>
-        <h5 className="last-result">Last result = {this.state.sum}</h5>
+      
         
         
         
